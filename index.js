a0d41d3d4c3235b5ded4ada9cbf9059fd1f8c139
Betarost <betarost@yandex.ru>
2023-06-13 13:16:52 +0300

    Try initMap

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const createElement = function (node) {
 const display = (node) => {
     const $app = document.getElementById("app")
     const newDom = createElement(node)
-    console.log('=56714b=', newDom)
+    // console.log('=56714b=', newDom)
     $app.appendChild(newDom)
 }
 
@@ -51,11 +51,19 @@ const load = async function (micro) {
 }
 
 const initMap = async function (tmp) {
-    console.log('=e37e54=', tmp)
+    // console.log('=e37e54=', tmp)
     for (let item of tmp.microFrontends) {
+        var head = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.id = "tmpcss";
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = `http://127.0.0.1:8000/assets/css/${item.name}.css`;
+        link.media = 'all';
+        head.appendChild(link);
         let t1 = await import(`http://127.0.0.1:8000/assets/js/${item.name}.js`)
         load(t1.micro)
-        console.log('=ae9b93=', t1.micro)
+        // console.log('=ae9b93=', t1.micro)
     }
 }
 
