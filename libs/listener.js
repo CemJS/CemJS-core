37b17f895698bbd5744e023dba420f7d62e0b3b8
Betarost <betarost@yandex.ru>
2023-08-07 15:28:20 +0300

    .

--- libs/listener.js
+++ libs/listener.js
@@ -1,34 +1,36 @@
 import { cemConfig } from './loader'
 import { Frontends, pageFront } from './class'
 
+
+const initFront = function (front) {
+    front.map((page, index) => {
+        if (Frontends.lists[page]) {
+            Frontends.lists[page].init(index)
+        }
+    })
+}
+
+const clearFront = function (front) {
+    pageFront.lists = pageFront.lists.filter((olPage, index) => {
+        if (!front.includes(olPage)) {
+            Frontends.lists[olPage]?.$el?.remove()
+            Frontends.lists[olPage].clearData()
+            return false
+        }
+        return true
+    })
+    initFront(front)
+}
+
 const changeUrl = async function (e) {
     for (let item of cemConfig.pages) {
         let checkReg = false
-        if (item.regex) {
-            let regex = new RegExp(item.regex)
-            if (window.location.pathname.search(new RegExp(item.regex)) != -1) {
-                checkReg = true
-            }
-        }
-        if ((item.url && item.url == window.location.pathname) || checkReg) {
-            pageFront.lists = pageFront.lists.filter((olPage, index) => {
-                if (!item.front.includes(olPage)) {
-                    Frontends.lists[olPage]?.$el?.remove()
-                    Frontends.lists[olPage].clearData()
-                    return false
-                }
-                return true
-            })
-
-            item.front.map((page, index) => {
-                if (Frontends.lists[page]) {
-                    Frontends.lists[page].init(index)
-                }
-            })
-            return
-
-        } else if (item.regex && item.regex == window.location.pathname) {
-
+        if (item.all) {
+            initFront(item.front)
+        } else if (item.regex && window.location.pathname.search(new RegExp(item.regex)) != -1) {
+            clearFront(item.front)
+        } else if (item.url && item.url == window.location.pathname) {
+            clearFront(item.front)
         }
     }
 }
